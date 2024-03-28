7071acdf06192fe57ff9323e752b31bf17b43c82
rawannmmohamed <rawanmelkhodary@gmail.com>
2024-03-28 21:28:29 +0200

    structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { UsersListComponent } from './pages/users-list/users-list.component';
+import { UserCardComponent } from './pages/user-card/user-card.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'users-list', pathMatch: 'full' },
+  { path: 'users-list', component: UsersListComponent, title: 'users list' },
+  { path: 'user-card', component: UserCardComponent, title: 'user' },
+];
